9788b45376822f5c4482cf4b89d2c17223c9bfbe
Jeff Cross <middlefloor@gmail.com>
2014-04-15 10:31:50 -0700

    chore: remove mocks from HttpBackend spec

--- test/httpBackend.spec.js
+++ test/httpBackend.spec.js
@@ -1,14 +1,9 @@
 import {$HttpBackend} from '../src/HttpBackend';
 import {Injector} from 'di/injector';
-import {$MockWindow} from './mocks/Window';
 import {use, inject} from 'di/testing';
 
 describe('$HttpBackend', function () {
   var $httpBackend;
-
-  beforeEach(function () {
-    use($MockWindow);
-  });
 
   beforeEach(inject($HttpBackend, function(_$httpBackend_) {
     $httpBackend = _$httpBackend_;

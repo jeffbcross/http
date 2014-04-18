dfe20c4421f2573e58fef9014775916605e5d74f
Jeff Cross <middlefloor@gmail.com>
2014-04-18 10:59:24 -0700

    feat(ConnectionMock): add more tests and make flush implementation more robust

--- test/ConnectionMock.spec.js
+++ test/ConnectionMock.spec.js
@@ -1,21 +1,11 @@
 import {$Http} from '../src/Http';
 import {assert} from 'assert';
 import {ConnectionMock, ConnectionMockBackend, ConnectionMockFactory, ResponseMap} from './mocks/ConnectionMock';
+import {IConnection} from '../src/IConnection';
 import {inject, use} from 'di/testing';
 import {PromiseBackend} from 'deferred/PromiseMock';
 
-describe('$ConectionMock', function() {
-  it('should')
-
-  describe('@Provide', function() {
-    it('should be injectable into $Http', function() {
-      use(ConnectionMockFactory);
-      inject($Http, function($http) {
-        expect($http.ConnectionClass).toBe(ConnectionMock);
-      });
-    });
-  });
-
+describe('ConnectionMockBackend', function() {
   describe('.whenRequest()', function() {
     it('should add the request signature to a class-level map', function() {
       ConnectionMockBackend.whenRequest('GET', '/users');
@@ -34,6 +24,67 @@ describe('$ConectionMock', function() {
   });
 
 
+  describe('.flush()', function() {
+    it('should throw if no requestResponseMap exists', function() {
+      expect(function() {
+        delete ConnectionMockBackend.requestResponseMap;
+        ConnectionMockBackend.flush();
+      }).toThrow(new ReferenceError('There are no responses to fulfill'));
+    });
+
+
+    it('should throw if no connections exist', function() {
+      expect(function() {
+        ConnectionMockBackend.requestResponseMap = new Map();
+        delete ConnectionMockBackend.connections;
+        ConnectionMockBackend.flush();
+      }).toThrow(new ReferenceError('There are no connections to resolve'));
+    });
+
+
+    it('should throw if connections list is of length zero', function() {
+      expect(function() {
+        ConnectionMockBackend.requestResponseMap = new Map();
+        ConnectionMockBackend.connections = [];
+        ConnectionMockBackend.flush();
+      }).toThrow(new ReferenceError('There are no connections to resolve'));
+    });
+
+
+    it('should call resolve() each connection with a matching response',
+        function() {
+          ConnectionMockBackend.forkZone().run(function() {
+            var requestResponseMap = new Map();
+            ConnectionMockBackend.whenRequest('GET', '/users').respond(200, 'User!');
+            var connection = new ConnectionMock();
+            var resolveSpy = spyOn(connection.deferred, 'resolve').and.callThrough();
+            connection.open('GET', '/users');
+            connection.send();
+            ConnectionMockBackend.flush();
+            expect(resolveSpy).toHaveBeenCalledWith('User!');
+          });
+        });
+
+
+    it('should ignore connections that have not yet executed', function() {
+      ConnectionMockBackend.forkZone().run(function() {
+        var requestResponseMap = new Map();
+        ConnectionMockBackend.whenRequest('GET', '/users').respond(200, 'User!');
+        var connection = new ConnectionMock();
+        var resolveSpy = spyOn(connection.deferred, 'resolve').and.callThrough();
+        connection.open('GET', '/users');
+        expect(ConnectionMockBackend.flush).toThrow();
+        expect(resolveSpy).not.toHaveBeenCalled();
+      });
+    });
+  });
+
+
+  describe('.addConnection', function() {
+
+  })
+
+
   describe('.verifyNotOustandingResponses', function() {
 
   });
@@ -42,7 +93,34 @@ describe('$ConectionMock', function() {
   describe('.verifyNoOutstandingRequests()', function() {
 
   });
+
+
+  describe('.forkZone()', function() {
+    it('should return a new zone from PromiseBackend', function() {
+      var forked = ConnectionMockBackend.forkZone();
+      assert.type(forked, Zone);
+      expect(forked.onZoneEnter.toString()).toContain('patchWithMock()');
+    });
+  })
 });
+
+
+describe('ConectionMock', function() {
+  it('should implement IConnection', function() {
+    assert.type(ConnectionMock, IConnection);
+  });
+
+
+  describe('@Provide', function() {
+    it('should be injectable into $Http', function() {
+      use(ConnectionMockFactory);
+      inject($Http, function($http) {
+        expect($http.ConnectionClass).toBe(ConnectionMock);
+      });
+    });
+  });
+});
+
 
 describe('ResponseMap', function() {
   describe('constructor', function() {
@@ -79,8 +157,7 @@ describe('misplaced integration tests', function() {
   it('should allow faking a response for a given method/url', function() {
     use(ConnectionMockFactory);
     inject($Http, function($http) {
-      var promiseBackend = new PromiseBackend();
-      promiseBackend.forkZone().run(function() {
+      ConnectionMockBackend.forkZone().run(function() {
         var response = '{"users":["Jeff"]}';
         var responseSpy = jasmine.createSpy();
         ConnectionMockBackend.whenRequest('GET', '/users').respond(200, response);
--- test/mocks/ConnectionMock.js
+++ test/mocks/ConnectionMock.js
@@ -1,21 +1,93 @@
 import {$ConnectionFactory} from '../../src/XHRConnection';
+import {Deferred} from 'deferred/Deferred';
+import {IConnection} from '../../src/IConnection';
 import {Provide} from 'di/annotations';
+import {PromiseBackend} from 'deferred/PromiseMock';
 
-export class MockConnection {
+export class ResponseMap extends Map {
   constructor() {
+    super();
+    this.set('body', null);
+    this.set('code', null);
+  }
 
+  respond(code:number, body:string) {
+    this.set('code', code);
+    this.set('body', body);
+  }
+}
+
+export class ConnectionMockBackend {
+  static flush() {
+    var responses, connections;
+    responses = ConnectionMockBackend.requestResponseMap;
+    connections = ConnectionMockBackend.connections;
+    if (!responses) {
+      throw new ReferenceError('There are no responses to fulfill');
+    }
+
+    if (!connections || !connections.length) {
+      throw new ReferenceError('There are no connections to resolve');
+    }
+
+    connections.forEach(function(connection) {
+      if (!connection.sent_) {
+        return;
+      }
+      if (responses.has(connection.method) &&
+          responses.get(connection.method).has(connection.url)) {
+        connection.deferred.resolve(responses.get(connection.method).
+            get(connection.url).
+            get('body'));
+      }
+    });
+
+    new PromiseBackend().flush(true);
+  }
+
+  static whenRequest(method:string, url:string) {
+    if (!ConnectionMockBackend.requestResponseMap) {
+      ConnectionMockBackend.requestResponseMap = new Map();
+    }
+    if (!ConnectionMockBackend.requestResponseMap.has(method)) {
+      ConnectionMockBackend.requestResponseMap.set(method, new Map());
+    }
+
+    var responseMap = new ResponseMap();
+    ConnectionMockBackend.requestResponseMap.get(method).set(url, responseMap);
+    return responseMap;
+  }
+
+  static addConnection(connection) {
+    if (!ConnectionMockBackend.connections) {
+      ConnectionMockBackend.connections = [];
+    }
+    ConnectionMockBackend.connections.push(connection);
+  }
+
+  static forkZone() {
+    return new PromiseBackend().forkZone();
+  }
+}
+
+export class ConnectionMock {
+  constructor() {
+    this.backend = new ConnectionMockBackend();
+    this.deferred = new Deferred();
+    ConnectionMockBackend.addConnection(this);
   }
 
   send() {
-
+    this.sent_ = true;
   }
 
-  open() {
-
+  open(method:string, url:string) {
+    this.method = method;
+    this.url = url;
   }
 
-  then() {
-
+  then(success, error) {
+    this.deferred.promise.then.call(this.deferred.promise, success, error);
   }
 
   success() {
@@ -25,11 +97,13 @@ export class MockConnection {
   error() {
 
   }
+
+
 }
 
 @Provide($ConnectionFactory)
-export class MockConnectionFactory {
+export class ConnectionMockFactory {
   constructor() {
-    return MockConnection;
+    return ConnectionMock;
   }
 }
